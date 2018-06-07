0d53f88c52eb64e8625a793138ac15087d5522ac
poliverjr <poliverjr@hotmail.com>
2018-06-07 00:44:45 -0500

    Initial commit

---
+++ app/src/main/java/poliv/jr/com/addcontact/ui/AddContactFragment.java
@@ -0,0 +1,11 @@
+package poliv.jr.com.addcontact.ui;
+
+import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
+
+public class AddContactFragment extends DialogFragment implements DialogInterface.OnShowListener{
+    @Override
+    public void onShow(DialogInterface dialog) {
+
+    }
+}
---
+++ app/src/main/java/poliv/jr/com/chatapp/login/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package poliv.jr.com.chatapp.login.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import poliv.jr.com.chatapp.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
