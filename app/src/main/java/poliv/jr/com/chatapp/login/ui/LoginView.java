a897a6eead5d8ad1723ee775072c7f34a9e86c3b
poliverjr <poliverjr@hotmail.com>
2018-06-14 12:16:13 -0500

    rdy for logic

---
+++ app/src/main/java/poliv/jr/com/chatapp/login/ui/LoginView.java
@@ -0,0 +1,17 @@
+package poliv.jr.com.chatapp.login.ui;
+
+public interface LoginView {
+    void enableInputs();
+    void disableInputs();
+    void showProgress();
+    void hideProgress();
+
+    void handleSignUp();
+    void handleSignIn();
+
+    void navigateToMainScreen();
+    void loginError(String error);
+
+    void newUserSuccess();
+    void newUserError(String error);
+}
