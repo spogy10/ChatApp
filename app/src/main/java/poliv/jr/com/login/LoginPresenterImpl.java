df0ceb82c58a7d1ee2e233b5e9b1fe37c7549ecf
poliverjr <poliverjr@hotmail.com>
2018-06-24 15:52:54 -0500

    making progress

--- app/src/main/java/poliv/jr/com/login/LoginPresenterImpl.java
+++ app/src/main/java/poliv/jr/com/login/LoginPresenterImpl.java
@@ -33,20 +33,82 @@ public class LoginPresenterImpl implements LoginPresenter {
             loginView.disableInputs();
             loginView.showProgress();
         }
+
+        loginInteractor.checkAlreadyAuthenticated();
     }
 
     @Override
     public void onEventMainThread(LoginEvent event) {
-        switch (event.getEventType())
+        switch (event.getEventType()){
+            case LoginEvent.onSignInError:
+                onSignError(event.getErrorMessage());
+                break;
+            case LoginEvent.onSignInSuccess:
+                onSignInSuccess();
+                break;
+            case LoginEvent.onSignUpError:
+                onSignUpError(event.getErrorMessage());
+                break;
+            case LoginEvent.onSignUpSuccess:
+                onSignUpSuccess();
+                break;
+            case LoginEvent.onFailedToRecoverSession:
+                onFailedToRecoverSession();
+                break;
+
+        }
+    }
+
+    private void onFailedToRecoverSession() {
+        if (loginView != null) {
+            loginView.hideProgress();
+            loginView.enableInputs();
+        }
+    }
+
+    private void onSignUpSuccess() {
+        if(loginView != null)
+            loginView.newUserSuccess();
+    }
+
+    private void onSignUpError(String errorMessage) {
+        if (loginView != null){
+            loginView.hideProgress();
+            loginView.enableInputs();
+            loginView.newUserError(errorMessage);
+        }
+    }
+
+    private void onSignInSuccess() {
+        if(loginView != null)
+            loginView.navigateToMainScreen();
+    }
+
+    private void onSignError(String errorMessage) {
+        if (loginView != null){
+            loginView.hideProgress();
+            loginView.enableInputs();
+            loginView.loginError(errorMessage);
+        }
     }
 
     @Override
     public void validateLogin(String email, String password) {
+        if(loginView != null){
+            loginView.disableInputs();
+            loginView.showProgress();
+        }
+        loginInteractor.doSignIn(email, password);
 
     }
 
     @Override
     public void registerNewUser(String email, String password) {
+        if(loginView != null){
+            loginView.disableInputs();
+            loginView.showProgress();
+        }
+        loginInteractor.doSignUp(email, password);
 
     }
 }
